044a64c70eaa9494deb64e5867107c4ad826900c
Mahtte94 <mahtias.jebrand@live.se>
2024-10-03 16:56:39 +0200

    added sorting by ranking
    $sortBy, $sortValues and an array_multisort

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 require_once __DIR__ . "/header.php";
 require_once __DIR__ . "/data.php";
 
+$sortBy = 'uefa-coefficient-ranking';
+
+$sortValues = [];
+foreach ($teams as $teamName => $teamInfo) {
+    $sortValues[] = $teamInfo[$sortBy];
+}
+
+array_multisort($sortValues, SORT_ASC, $teams);
+
 ?>
+
+
 
 <main>
 
     <?php
 
     foreach ($teams as $teamName => $teamInfo) { ?>
+
         <h2><?= $teamName ?></h2>
         <p>
             <?= ucwords('league') . ": " . $teamInfo['league']; ?>
@@ -23,7 +35,7 @@ require_once __DIR__ . "/data.php";
         <a href="<?= $teamInfo['url']; ?>">
             <?= $teamName; ?>
         </a>
-        <img src="<?= $teamInfo['logo']?>"/>
+        <img src="<?= $teamInfo['logo'] ?>" />
         <p><?= ucwords('group') . ": " . $teamInfo['group']; ?></p>
 
     <?php
