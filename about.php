3964695c39804ba0662bcb02e6b8d2be69721046
Mahtte94 <mahtias.jebrand@live.se>
2024-10-03 13:59:05 +0200

    added foreach

--- about.php
+++ about.php
@@ -12,3 +12,5 @@ require_once __DIR__ . "/header.php";
 <?php
 
 require_once __DIR__ . "/footer.php";
+
+?>
--- index.php
+++ index.php
@@ -2,11 +2,40 @@
 
 require_once __DIR__ . "/header.php";
 require_once __DIR__ . "/data.php";
-require_once __DIR__ . "/footer.php";
-
 
 ?>
 
 <main>
 
+    <?php
+
+    foreach ($teams as $teamName => $teamInfo) { ?>
+        <h2><?= $teamName ?></h2>
+        <p>
+            <?= ucwords('league') . ": " . $teamInfo['league']; ?>
+        </p>
+        <p>
+            <?= ucwords('uefa-coefficient-ranking') . ": " . $teamInfo['uefa-coefficient-ranking'];  ?>
+        </p>
+        <p>
+            <?= ucwords('city') . ": " . $teamInfo['city'];  ?>
+        </p>
+        <a href="<?= $teamInfo['url']; ?>">
+            <?= $teamName; ?>
+        </a>
+        <img src="<?= $teamInfo['logo']?>"/>
+        <p><?= ucwords('group') . ": " . $teamInfo['group']; ?></p>
+
+    <?php
+    }
+
+    ?>
+
+
 </main>
+
+<?php
+
+require_once __DIR__ . "/footer.php";
+
+?>
